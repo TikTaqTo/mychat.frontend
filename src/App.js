4221e1bf30b5089b9b2b6701d674a7ecc18321b6
TikTaqTo <adai.02_kz@mail.ru>
2023-08-16 00:19:13 +0600

    Initial Commit(Сверстал форму входа в аккаунт)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, Block } from './components'
+import { Form, Input } from 'antd';
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
+import './styles/index.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <section className="auth">
+        <div className="auth__content">
+        <div className="auth__top">
+          <h2>Войти в аккаунт</h2>
+          <p>Пожалуйста, войдите в свой аккаунт</p>
+        </div>
+        <Block>
+          <Form className="login-form">
+            <Form.Item>
+              <Input
+                id="email"
+                prefix={<MailOutlined style={{ color: "rgba(0,0,0,.25)" }}  />}
+                size="large"
+                placeholder="E-Mail"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input
+                id="password"
+                prefix={<LockOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+                size="large"
+                type="password"
+                placeholder="Пароль"/>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                size="large"
+              >
+                Войти в аккаунт
+              </Button>
+            </Form.Item>
+            <a className="auth__register-link" href='#'>
+              Зарегистрироваться
+            </a>
+          </Form>
+        </Block>
+        </div>
+      </section>
     </div>
   );
 }
